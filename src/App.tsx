2ae61c98716f95df2fae0662f709d07a36356688
trlol <golovanovk5555@gmail.com>
2025-05-28 10:33:39 +0300

    Revert "Merge pull request #1 from trlol/ka.golovanov/template"
    This reverts commit f7ff03c6172e6f735175849bb4abddf46381bbc0, reversing
    changes made to 72246e599c81d7df8c5834cc91fc31d8567abc41.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SearchResultsPage from './pages/SearchResultsPage';
 
-function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchResultsPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
